70767f1bfe15c4ae7fd3d07fe7bf779c1da188e2
bsilakaymak <basaksilakaymak@gmail.com>
2023-09-14 10:45:33 +0200

    Some refactoring

--- jat-fe/src/components/JobListing.tsx
+++ jat-fe/src/components/JobListing.tsx
@@ -1,11 +1,22 @@
 const JobListing = ({ job }: { job: any }) => {
+  const getBgColor = () => {
+    switch (job.status) {
+      case "Application rejected":
+        return "bg-listingClosed";
+      case "Got an offer":
+        return "bg-listingAccepted";
+      default:
+        return "bg-listingPending";
+    }
+  };
+  console.log(job);
   return (
     <a
       key={job.jobId}
       href={`/job/${job.jobId}`}
       className="max-w-sm rounded overflow-hidden bg-textPrimary shadow-lg border-solid border-2 border-primary hover:opacity-70"
     >
-      <div className="px-6 py-4">
+      <div className={`px-6 py-4 bg-opacity-20 ${getBgColor()}`}>
         <p className="font-bold text-xl mb-2">{job.jobTitle}</p>
         <p>Company: {job.companyName}</p>
         <p className="text-red-900">Status: {job.status}</p>
--- jat-fe/tailwind.config.js
+++ jat-fe/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         secondary: "#3C2A21",
         textPrimary: "#D5CEA3",
         textSecondary: "#E5E5CB",
+        listingClosed: "#8a1a30",
+        listingPending: "#baa729",
+        listingAccepted: "#1d8038",
       },
       fontFamily: {
         sans: ["Graphik", "sans-serif"],
